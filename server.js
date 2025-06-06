5847e4d9e8147109ffceb23c9d657e2c147c02ba
muskan02062000 <sharmamuskan262000@gmail.com>
2025-06-06 17:06:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const dotenv=require("dotenv");
+const gameRoutes=require("./routes/game");
+dotenv.config();
+const app=express();
+app.use(express.json());
+mongoose.connect(process.env.MONGO_URI)
+  .then(()=>console.log(err));
+app.use("api/game",gameRoutes);
 
+const PORT=process.env.PORT||5000;
+app.listen(PORT,()=>{
+  console.log(`Server running on port ${PORT}`);
+})
