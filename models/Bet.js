8930040797ff119b7be77b27610d1c8ec7ba9947
muskan02062000 <sharmamuskan262000@gmail.com>
2025-06-06 17:12:52 +0530

    Update Bet.js

--- models/Bet.js
+++ models/Bet.js
@@ -1,1 +1,11 @@
-
+const mongoose=require("mongoose");
+const BetSchema=new mongoose.Schema({
+  userName:String,
+  amount:Number,
+  betOn:{
+    type:String,
+    winAmount:Number,
+    timeStamp:{
+      type:Date,
+      default:Date.now}});
+module.exports=mongoose.model("Bet",BetSchema);
