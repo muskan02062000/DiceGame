a091696717900b8f7c113282ffd1aa9955098f2d
muskan02062000 <sharmamuskan262000@gmail.com>
2025-06-06 17:10:10 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,10 @@
-
+const mongoose=require("mongoose");
+const connectDB=async()=>{
+  try{
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("mongodb connected")}
+catch(err){
+  console.err(err.message);
+  process.exit(1)
+}}
+module.exports=connectDB;
